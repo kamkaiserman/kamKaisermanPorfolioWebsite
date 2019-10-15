0349eed58b07761b330a8525a840939374637654
Kam Kaiserman <43228122+kamkaiserman@users.noreply.github.com>
2019-10-15 00:24:45 -0600

    Added all files.
    This draft still needs the mobile navbar fixed.

---
+++ navbar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("navbar-mobile").style.height = "100%";
+    document.getElementById("navbar-mobile").style.opacity = "1";
+}
+
+function closeNav() {
+    document.getElementById("navbar-mobile").style.height = "0%";
+    document.getElementById("navbar-mobile").style.opacity = "0";
+}
